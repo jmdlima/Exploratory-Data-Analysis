98f8415f6e29c0e48116adaef936d79f30e38b76
Joseph D <jmdlima@gmail.com>
2015-08-10 02:32:08 +1200

    Made small changes to the PrepareData

--- PrepareData.R
+++ PrepareData.R
@@ -1,5 +1,10 @@
+setwd("..")
+setwd("..")
+
 HPC<-read.table("Datasets/household_power_consumption.txt", sep=";",na.strings="?",header=T )
 HPC$Date<-as.Date(HPC$Date,"%d/%m/%Y")
 
 data <- subset(HPC,HPC$Date %in% as.Date(c("2007-02-01","2007-02-02")))
-data$date_time <- as.POSIXct(paste(data$Date,data$Time))
+data$datetime <- as.POSIXct(paste(data$Date,data$Time))
+
+setwd("Repositories/Exploratory-Data-Analysis")
